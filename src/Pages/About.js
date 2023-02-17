d1ffe81f81a67b91a67321b68f3702e50c551085
Phil <3266880@gmail.com>
2023-02-17 18:24:36 +0200

    changes
    change carr

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -45,7 +45,7 @@ export default class About extends Component {
                   </p>
                 </Tab.Pane>
                 <Tab.Pane eventKey="third">
-                  <img src="https://www.zdnet.com/a/img/resize/0a6b0be2f543ddbf313fc83a706b807b77c3c202/2021/07/19/8a337c80-5ed6-43a1-98fb-b981d420890f/programming-languages-shutterstock-1680857539.jpg?auto=webp&fit=crop&height=900&width=1200"></img>
+                  <img src="https://www.onlc.com/blog/wp-content/uploads/2017/07/ONLC-2017-4.png"></img>
                   <p>
                     Enim ut sem viverra aliquet eget sit amet tellus cras
                     adipiscing enim eu turpis egestas pretium aenean pharetra
